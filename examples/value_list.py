18b4181744f21fd0203ad9f8d2103d5d95246329
root <showell30@yahoo.com>
2023-03-19 13:05:54 +0000

    Add add.

--- examples/value_list.py
+++ examples/value_list.py
@@ -9,10 +9,12 @@ def enforce_list_types(lst, _type):
 
 
 def enforce_math_protocol(math):
+    assert hasattr(math, "add")
     assert hasattr(math, "additive_inverse")
     assert hasattr(math, "multiply_by_constant")
     assert hasattr(math, "value_type")
     assert hasattr(math, "zero")
+    assert callable(math.add)
     assert callable(math.additive_inverse)
     assert callable(math.multiply_by_constant)
     assert type(math.zero) == math.value_type
@@ -47,13 +49,14 @@ class ValueList:
         return self.new([additive_inverse(elem) for elem in lst])
 
     def add_with(self, other):
+        add = self.math.add
         zero = self.math.zero
         lst1 = self.lst
         lst2 = other.lst
         # do the analog of elementary school arithmetic
         new_size = max(len(lst1), len(lst2))
         return self.new(
-            [arr_get(lst1, i, zero) + arr_get(lst2, i, zero) for i in range(new_size)]
+            [add(arr_get(lst1, i, zero), arr_get(lst2, i, zero)) for i in range(new_size)]
         )
 
     def enforce_partner_type(self, other):
@@ -96,6 +99,7 @@ class ValueList:
 if __name__ == "__main__":
 
     class IntegerMath:
+        add = lambda a, b: a + b
         additive_inverse = lambda a: -1 * a
         multiply_by_constant = lambda a, b: a * b
         value_type = int
@@ -130,6 +134,7 @@ if __name__ == "__main__":
     from number_list import NumberList
 
     class NumberListMath:
+        add = lambda a, b: a + b
         additive_inverse = lambda a: -a
         multiply_by_constant = lambda c, num_list: NumberList(
             NumberList.mul_by_constant(c, num_list.lst)
